20f2fa5b417cf2a325bf9a99f5737e8a414b9d69
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-04-08 20:13:40 -0400

    5.6. Links com URL Expression

--- src/main/java/tech/bacuri/brewer/controller/CervejaController.java
+++ src/main/java/tech/bacuri/brewer/controller/CervejaController.java
@@ -8,7 +8,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tech.bacuri.brewer.model.Cerveja;
 
@@ -37,11 +36,5 @@ public class CervejaController {
         attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
         System.out.println(">>> sku: " + cerveja.getSku());
         return "redirect:/cervejas/novo";
-    }
-
-    @GetMapping("/cadastro-produto")
-    public String cadastroProduto(Model model) {
-
-        return "cerveja/cadastro-pro duto";
     }
 }
