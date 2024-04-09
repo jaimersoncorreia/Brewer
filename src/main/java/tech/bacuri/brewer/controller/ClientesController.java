9e4adc2952bd229aea2d5af705d2b8a7de977c72
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-04-09 19:14:28 -0400

    5.10. Desafio: Prototipando o cadastro de cliente

---
+++ src/main/java/tech/bacuri/brewer/controller/ClientesController.java
@@ -0,0 +1,14 @@
+package tech.bacuri.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClientesController {
+    @GetMapping("/novo")
+    public String novo() {
+        return "cliente/CadastroCliente";
+    }
+}
