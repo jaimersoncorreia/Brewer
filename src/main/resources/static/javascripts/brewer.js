d81fcb6cbe61d9903f998de36c6d6678e2f96fb0
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-04-08 21:45:16 -0400

    12.2. Modularizando a máscara de dinheiro

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,20 @@
-$(function() {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney();
-	
-	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0 });
+var Brewer = Brewer || {}
+
+Brewer.MaskMoney = (function () {
+    function MaskMoney() {
+        this.decimal = $('.js-decimal');
+        this.plain = $('.js-plain');
+    }
+
+    MaskMoney.prototype.enable = function () {
+        this.decimal.maskMoney();
+        this.plain.maskMoney({precision: 0});
+    }
+
+    return MaskMoney;
+})();
+
+$(function () {
+    var maskMoney = new Brewer.MaskMoney();
+    maskMoney.enable();
 });
