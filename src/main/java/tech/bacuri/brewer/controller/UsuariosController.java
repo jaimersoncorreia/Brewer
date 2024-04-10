b57b154739ae4a2b08a0b4617b38f2f14f6e3f84
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-04-09 20:04:30 -0400

    5.11. Desafio: prototipando o cadastro de usuário 5.12. Bootstrap Switch no status do usuário

---
+++ src/main/java/tech/bacuri/brewer/controller/UsuariosController.java
@@ -0,0 +1,15 @@
+package tech.bacuri.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuariosController {
+
+    @GetMapping("/novo")
+    public String novo() {
+        return "usuario/CadastroUsuario";
+    }
+}
